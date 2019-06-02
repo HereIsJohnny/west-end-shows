3cdb195d520e6f3d525984add47256223fb030c9
Maciej Kowalski <maciejkowalski@MacBook-Pro-Maciej.local>
2019-06-02 12:04:46 +0200

    movie list item on mobile

--- src/app/ApplicationRoot.tsx
+++ src/app/ApplicationRoot.tsx
@@ -1,8 +1,10 @@
+import { CssBaseline, makeStyles } from '@material-ui/core'
+import Container from '@material-ui/core/Container'
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
-import { getMovies } from './store/movies/actions'
 import { Routes } from './Routes'
+import { getMovies } from './store/movies/actions'
 
 export interface ApplicationRootActions {
   onApplicationStart: () => void
@@ -15,16 +17,27 @@ const connectComponent = connect(
   })
 )
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.background.default,
+  },
+}))
+
 export const ApplicationRoot = connectComponent(
   ({ onApplicationStart }: ApplicationRootActions) => {
     useEffect(() => {
       onApplicationStart()
     }, [])
 
+    const classes = useStyles()
+
     return (
-      <div>
-        <Routes />
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <Container className={classes.root}>
+          <Routes />
+        </Container>
+      </React.Fragment>
     )
   }
 )
--- src/app/components/MoviesList/MoviesList.tsx
+++ src/app/components/MoviesList/MoviesList.tsx
@@ -1,19 +1,57 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Typography,
+} from '@material-ui/core'
 import { Movie } from 'app/store/movies/reducer'
 import React from 'react'
 
 export const MoviesList: React.FunctionComponent<{ movies: Movie[] }> = ({ movies }) => (
-  <div>
+  <React.Fragment>
     {movies.map(movie => (
       <MovieCard key={movie.title} movie={movie} />
     ))}
-  </div>
+  </React.Fragment>
 )
 
-const MovieCard: React.FunctionComponent<{ movie: Movie }> = ({ movie }) => (
-  <div>
-    <img src={movie.imageSrc} />
-    <h2>{movie.title}</h2>
-    <h3>{movie.location}</h3>
-    <p>{movie.description}</p>
-  </div>
-)
+const useStyles = makeStyles({
+  button: {
+    marginLeft: 'auto',
+  },
+  media: {
+    height: 140,
+  },
+})
+
+const MovieCard: React.FunctionComponent<{ movie: Movie }> = ({ movie }) => {
+  const classes = useStyles()
+
+  return (
+    <Box mt={4} mb={4}>
+      <Card>
+        <CardMedia className={classes.media} image={movie.imageSrc} title={movie.title} />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='h2'>
+            {movie.title}
+          </Typography>
+          <Typography gutterBottom variant='h5' component='h3'>
+            {movie.location}
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            {movie.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button className={classes.button} size='large' color='primary'>
+            Get tickets
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+  )
+}
